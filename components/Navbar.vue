08ce4c59410b04079c5ec2f2b47e88fc00f6b4e3
Erik Beem <erik.beem@hotmail.com>
2021-11-26 16:52:43 +0100

    add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-row container">
+        <Navlink />
+        <Navlink to="/contact" page-name="Contact" />
+    </div>
+</template>
+<script>
+import Navlink from "./Navlink.vue";
+export default {
+    components: { Navlink }
+}
+</script>
---
+++ components/Navlink.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <nuxt-link :class="styles" :to="to">{{ pageName }}</nuxt-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Navlink',
+    props: {
+        to: {
+            type: String,
+            default: '/',
+        },
+        pageName: {
+            type: String,
+            default: 'Home',
+        },
+        styles: {
+            type: String,
+            default: 'text-xl p-2'
+        }
+    }
+}
+</script>
+<style>
+.nuxt-link-exact-active {
+    color: rgb(145, 145, 255);
+}
+</style>
