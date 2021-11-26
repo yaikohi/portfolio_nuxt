93ccc8f5f6d76dda7105eedaff55c1faba610ee0
Erik Beem <erik.beem@hotmail.com>
2021-11-26 16:52:37 +0100

    add list

---
+++ components/ListPoint.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <slot />
+    </li>
+</template>
+<script lang="ts">
+export default {
+    name: 'ListPoint',
+    props: {
+        content: {
+            type: String,
+            default: "Hi"
+        }
+    }
+}
+</script>
