b3b47d5697b71b066179a67c1ec220cad7d5661c
Erik Beem <erik.beem@hotmail.com>
2021-11-26 16:52:32 +0100

    add header

---
+++ components/HeroHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 :class="styles">{{ content }}</h1>
+</template>
+<script lang="ts">
+export default {
+    name: 'Heroheader',
+    props: {
+        styles: {
+            type: String,
+            default: "font-hero text-black bg-white text-left uppercase text-6xl p-4 m-4"
+        },
+        content: {
+            type: String,
+            default: "junior developer, \n student"
+        }
+    }
+}
+</script>
