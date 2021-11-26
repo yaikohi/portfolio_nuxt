6f7883425708d673f267ca571aaf0673551eb260
Erik Beem <erik.beem@hotmail.com>
2021-11-26 16:53:01 +0100

    add paragraph

---
+++ components/Paragraph.vue
@@ -0,0 +1,12 @@
+<template>
+    <p class="font-light font-paragraph text-2xl">{{ content }}</p>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Paragraph',
+    props: {
+        content: { type: String, default: "Hello" },
+    }
+}
+</script>
