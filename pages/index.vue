e8250e95c95cfc98dcd3a11f6948f7c88eb11886
Erik Beem <erik.beem@hotmail.com>
2021-11-26 16:53:29 +0100

    style land page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial/>
+  <div>
+    <hero-header />
+    <paragraph content="Erik - that's my name 😁. " />
+    <paragraph content="I am... " />
+    <li>a student at the Hogeschool Utrecht</li>
+    <li>looking for an internship position as front-end software developer.</li>
+  </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
