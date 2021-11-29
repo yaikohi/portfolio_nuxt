a5cef63b954c23cb3b56c9441619540885e8c789
Erik Beem <erik.beem@hotmail.com>
2021-11-29 10:31:25 +0100

    remove unused component

--- components/UnList.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <ul class="list-disc p-4">
-        <list-point>I love learning</list-point>
-        <list-point>I am good at communication</list-point>
-        <list-point>I'm experienced with scrum and agile methodologies</list-point>
-        <list-point>I want to make cool stuff and learn how to do them well.</list-point>
-    </ul>
-</template>
