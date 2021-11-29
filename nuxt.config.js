f4b35b618ad175d9c69b6e1c291c65dd50920112
Erik Beem <erik.beem@hotmail.com>
2021-11-29 10:59:20 +0100

    update style

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'freewall',
+    title: 'Erik Beem',
     htmlAttrs: {
       lang: 'en',
     },
